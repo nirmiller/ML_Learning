5d3ff674b02e8ce2fca83fe732ed90d0b5ce88e2
nirmiller <52181530+nirmiller@users.noreply.github.com>
2020-10-08 23:39:42 -0500

    ResNet50 Code

---
+++ ResNetTest.py
@@ -0,0 +1,62 @@
+from tensorflow.keras.layers import Conv2D,Flatten, Dense, MaxPool2D, BatchNormalization, GlobalAveragePooling2D
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import Model
+from tensorflow.keras.models import load_model
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sb
+import tensorflow as tf
+import os
+
+train_data_dir = r"FlowerDataSet\processed_data\train"
+img_height, img_width = (224, 224)
+batch_size = 32
+train_datagen = ImageDataGenerator(preprocessing_function=preprocess_input,
+                                   shear_range=.2,
+                                   zoom_range=.2,
+                                   horizontal_flip=True,
+                                   validation_split=.4)
+valid_data_dir = r"FlowerDataSet\processed_data\val"
+train_generator = train_datagen.flow_from_directory( train_data_dir,
+                                                     target_size=(img_height, img_width),
+                                                     batch_size=batch_size,
+                                                     class_mode='categorical',
+                                                     subset='training')
+test_generator = train_datagen.flow_from_directory( valid_data_dir,
+                                                     target_size=(img_height, img_width),
+                                                     batch_size=1,
+                                                     class_mode='categorical',
+                                                     subset='validation')
+
+save_path = b"C:\Users\slice\PycharmProjects\ResNetKMS\Model"
+
+model = tf.keras.models.load_model("Model/ResNet_50Flowers")
+
+
+filenames = test_generator.filenames
+nb_samples = len(test_generator)
+y_prob = []
+y_act = []
+test_generator.reset()
+for _ in range(nb_samples):
+    X_test, Y_test = test_generator.next()
+    y_prob.append(model.predict(X_test))
+    y_act.append(Y_test)
+
+predicted_class = [list(train_generator.class_indices.keys())[i.argmax()] for i in y_prob]
+actual_class = [list(train_generator.class_indices.keys())[i.argmax()] for i in y_act]
+
+out_df = pd.DataFrame(np.vstack([predicted_class, actual_class]).T, columns=['predicted_class', 'actual_class'])
+confusion_matrix = pd.crosstab(out_df['actual_class'], out_df['predicted_class'], rownames=['Actual'], colnames=['Predicted'])
+print("load heatmap")
+
+sb.heatmap(confusion_matrix, cmap= 'Blues', annot=True,fmt='d')
+plt.show()
+
+print('test accuarcy : {}'.format((np.diagonal(confusion_matrix).sum()/confusion_matrix.sum().sum()*100)))
+
---
+++ resnet.py
@@ -0,0 +1,80 @@
+from tensorflow.keras.layers import Conv2D,Flatten, Dense, MaxPool2D, BatchNormalization, GlobalAveragePooling2D
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import Model
+from tensorflow.keras.models import load_model
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+import tensorflow as tf
+
+
+save_path = b"C:\Users\slice\PycharmProjects\ResNetKMS\Model"
+
+#dimensions
+img_height, img_width = (224, 224)
+batch_size = 32
+
+#pre-processing images
+train_data_dir = r"FlowerDataSet\processed_data\train"
+valid_data_dir = r"FlowerDataSet\processed_data\val"
+test_data_dir = r"FlowerDataSet\processed_data\test"
+
+train_datagen = ImageDataGenerator(preprocessing_function=preprocess_input,
+                                   shear_range=.2,
+                                   zoom_range=.2,
+                                   horizontal_flip=True,
+                                   validation_split=.4)
+
+train_generator = train_datagen.flow_from_directory( train_data_dir,
+                                                     target_size=(img_height, img_width),
+                                                     batch_size=batch_size,
+                                                     class_mode='categorical',
+                                                     subset='training')
+valid_generator = train_datagen.flow_from_directory( valid_data_dir,
+                                                     target_size=(img_height, img_width),
+                                                     batch_size=batch_size,
+                                                     class_mode='categorical',
+                                                     subset='validation')
+
+
+
+test_generator = train_datagen.flow_from_directory( valid_data_dir,
+                                                     target_size=(img_height, img_width),
+                                                     batch_size=1,
+                                                     class_mode='categorical',
+                                                     subset='validation')
+
+x,y = test_generator.next()
+print(x.shape)
+
+base_model = ResNet50(include_top = False, weights = 'imagenet')
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dense(1024, activation='relu')(x)
+
+#Number of classes V
+predictions = Dense(train_generator.num_classes, activation='softmax')(x)
+model = Model(inputs=base_model.input, outputs=predictions)
+
+
+
+for layer in base_model.layers:
+    layer.trainable =False
+
+#Finale layer
+model.compile(optimizer='adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+
+model.fit(train_generator, epochs=1)
+
+model.save(b'Model\ResNet_50Flowers')
+
+
+#model = load_model(os.path.join(save_path, b'ResNet_50Flowers'))
+
+test_loss, test_acc = model.evaluate(test_generator, verbose = 2)
+print('\nTest Accuarcy', test_acc)
